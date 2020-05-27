4451bcd0df2af5cebd3b99bc769a7641f761ab70
brandon.authenreith <brandonauth@gmail.com>
2020-05-26 22:55:50 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+// 1
+
+function add(x, y) {
+    return x+y;
+}
+console.log(add(1, 8))
+
+//2
+
+function mult (c, d) {
+    let multi = c;
+    for (let i = 1; i < d; i++) {
+        multi = add (c, multi);
+    }
+    return multi;
+}
+console.log(mult(6, 5))
+
+//3
+
+function exp (g, h) {
+    let base = g;
+    for (let i = 1; i < h; i++) {
+        base = mult(g, base); 
+    }
+    return base;
+}
+console.log(exp(2,8))
+
+//4
+
+function factorial (z) {
+    let fact = 1
+    for (let i = 1; i<= z; i++ ) {
+        fact = mult(i, fact);
+    }
+
+    return fact;
+}
+console.log(factorial(8))
